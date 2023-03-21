0a4ccf7e76cd778feff18db9f6b50ee5d881a0a1
jmvsevilleja <jmvsevilleja@gmail.com>
2023-03-21 17:57:26 +0800

    Added react ts tailwind router

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline text-red-600">
+          React Typescript Tailwind Template
+        </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
